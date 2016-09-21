0410acc9df0391aaf67f74da8ef2a1d62d53b02a
akuzmin96 <akuzmin@unm.edu>
2016-09-21 15:58:54 -0600

    Doing neighbors

--- src/Cell.java
+++ src/Cell.java
@@ -6,11 +6,15 @@ public class Cell
 
   private boolean alive;
   private Cell[][][] board;
+  private int x, y, z;
 
   public Cell(boolean alive, Cell[][][] board, int x, int y, int z)
   {
     this.alive = alive;
     this.board = board;
+    this.x = x;
+    this.y = y;
+    this.z = z;
   }
 
   public void setAlive(boolean state)
@@ -23,12 +27,26 @@ public class Cell
     return alive;
   }
 
-  public int getNumberOfNeighbors()
+  public int getNeighbors()
   {
     int neighbors = 0;
+    neighbors += getNeighborsHelper(x - 1, y, z);
+    neighbors += getNeighborsHelper(x + 1, y, z);
+    neighbors += getNeighborsHelper(x, y - 1, z);
+    neighbors += getNeighborsHelper(x, y + 1, z);
+    neighbors += getNeighborsHelper(x, y , z - 1);
+    neighbors += getNeighborsHelper(x, y , z + 1);
 
-    for(int )
 
     return neighbors;
+  }
+
+  private int getNeighborsHelper(int xAxis, int yAxis, int zAxis)
+  {
+    if(board[xAxis][yAxis][zAxis].getAlive())
+    {
+      return 1;
+    }
+    return 0;
   }
 }
