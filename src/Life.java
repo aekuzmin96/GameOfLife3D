0a43e30512745eab41cd249776aee32d419717b6
akuzmin96 <akuzmin@unm.edu>
2016-09-22 00:43:20 -0600

    Working on GUI

--- src/Life.java
+++ src/Life.java
@@ -5,8 +5,11 @@
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 import java.awt.*;
@@ -18,6 +21,10 @@ public class Life extends Application implements EventHandler<ActionEvent>
   private Cell[][][] currentState = new Cell[31][31][31];
   private Cell[][][] nextState = new Cell[31][31][31];
   final Group root = new Group();
+  private int r1;
+  private int r2;
+  private int r3;
+  private int r4;
 
   @Override
   public void start(Stage primaryStage)
@@ -26,6 +33,21 @@ public class Life extends Application implements EventHandler<ActionEvent>
     primaryStage.setTitle("Game of Life in 3D");
     primaryStage.setScene(scene);
 
+    GridPane grid = new GridPane();
+    grid.setAlignment(Pos.TOP_RIGHT);
+    grid.setHgap(15);
+    grid.setVgap(15);
+
+    TextField r1Text = new TextField("Enter r1 value");
+    TextField r2Text = new TextField("Enter r2 value");
+    TextField r3Text = new TextField("Enter r3 value");
+    TextField r4Text = new TextField("Enter r4 value");
+
+
+    grid.add(r1Text);
+
+
+    root.getChildren().add(grid);
     initializeGame();
     primaryStage.show();
   }
